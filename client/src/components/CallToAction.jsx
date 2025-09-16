84dae9cf1df02b28ce1e790c5dd0a0b0b3c4ea35
Priyatam Shaw <priyatamshaw@gmail.com>
2025-09-16 14:37:27 +0530

    issues fixed

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -5,14 +5,16 @@ import { ArrowRight } from 'lucide-react'
 
 const CallToAction = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-around text-sm border border-gray-200 rounded-2xl m-auto max-w-5xl w-full bg-indigo-300 mb-16">
+    <div className='px-4 sm:px-20 xl:px-32 py-28'>
+
+    <div className="flex flex-col md:flex-row items-center justify-around text-sm border border-gray-200 rounded-2xl m-auto max-w-5xl w-full bg-gray-50">
                 
             <div className="flex flex-col text-center md:text-left items-center md:items-start pt-14 md:p-10">
                 <h2 className="md:text-4xl text-2xl font-bold text-gray-800">Boost your productivity.<br />Start using our app today.</h2>
         
                 <div className="flex items-center gap-4 mt-6">
                     <Link to='/ai' type="button" aria-label="getStarted" className="bg-primary  px-7 py-2.5 text-white rounded-full active:scale-95 transition-all">
-                        Get started
+                        Get started free
                     </Link>
                     <Link to='/about-us' type="button" className="group flex items-center gap-2 px-7 py-2.5 active:scale-95 transition">
                         Learn more
@@ -23,6 +25,7 @@ const CallToAction = () => {
         
             <img className="max-w-[375px] pt-10 md:p-0" src="/excitedWomenImage.png" alt="excitedWomenImage" />
         </div>
+    </div>
   )
 }
 
